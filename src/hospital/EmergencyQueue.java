6f061a1ffd43ef888471031034fd0e86c9a41c38
23da2-0687-cmd <23da2-0687@sltc.ac.lk>
2026-09-02 12:44:45 +0530

    Added enqueue, dequeue, display, and empty queue handling

--- src/hospital/EmergencyQueue.java
+++ src/hospital/EmergencyQueue.java
@@ -13,4 +13,59 @@ public class EmergencyQueue {
         rear = -1;
         size = 0;
     }
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public boolean isFull() {
+        return size == queue.length;
+    }
+
+    public void enqueue(Patient patient) {
+
+        if (isFull()) {
+            System.out.println("Queue is full.");
+            return;
+        }
+
+        rear = (rear + 1) % queue.length;
+        queue[rear] = patient;
+        size++;
+
+        System.out.println("Patient added to emergency queue.");
+    }
+
+    public Patient dequeue() {
+
+        if (isEmpty()) {
+            System.out.println("Queue is empty.");
+            return null;
+        }
+
+        Patient patient = queue[front];
+        queue[front] = null;
+
+        front = (front + 1) % queue.length;
+        size--;
+
+        return patient;
+    }
+
+    public void display() {
+
+        if (isEmpty()) {
+            System.out.println("Emergency queue is empty.");
+            return;
+        }
+
+        System.out.println("\nEmergency Patient Queue:");
+
+        for (int i = 0; i < size; i++) {
+
+            int index = (front + i) % queue.length;
+
+            System.out.println("----------------------");
+            queue[index].display();
+        }
+    }
 }
