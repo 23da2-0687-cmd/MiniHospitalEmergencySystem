ce62ab51a927a0874022716beff19a3d30005cda
23da2-0687-cmd <23da2-0687@sltc.ac.lk>
2026-09-02 12:59:47 +0530

    Added push, pop, display, and empty stack handling

--- src/hospital/TreatmentStack.java
+++ src/hospital/TreatmentStack.java
@@ -9,4 +9,48 @@ public class TreatmentStack {
         stack = new String[capacity];
         top = -1;
     }
+
+    public boolean isEmpty() {
+        return top == -1;
+    }
+
+    public boolean isFull() {
+        return top == stack.length - 1;
+    }
+
+    public void push(String treatment) {
+
+        if (isFull()) {
+            System.out.println("Treatment stack is full.");
+            return;
+        }
+
+        stack[++top] = treatment;
+
+        System.out.println("Treatment added.");
+    }
+
+    public String pop() {
+
+        if (isEmpty()) {
+            System.out.println("Treatment stack is empty.");
+            return null;
+        }
+
+        return stack[top--];
+    }
+
+    public void display() {
+
+        if (isEmpty()) {
+            System.out.println("Treatment history is empty.");
+            return;
+        }
+
+        System.out.println("\nTreatment History:");
+
+        for (int i = top; i >= 0; i--) {
+            System.out.println((top - i + 1) + ". " + stack[i]);
+        }
+    }
 }
